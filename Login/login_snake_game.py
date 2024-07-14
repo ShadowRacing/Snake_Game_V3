24ec097089212716221e89e50d3ce31c5e109182
ShadowRacing <wimjr.roelofsen@gmail.com>
2024-07-14 12:05:58 +0200

    Added logins screen
    Added login screen
    Forgot password
    create user
    and show who the user is

--- Login/login_snake_game.py
+++ Login/login_snake_game.py
@@ -4,18 +4,17 @@ File: login_example.py
 
 import json
 import os
+import traceback
 import customtkinter as ctk
-
-# Define the file where user data will be stored
-USER_DATA_FILE = 'users.json'
 
 # Function to read user data from file
 class LoginAndUserScreen():
     """
     Class to create a login screen with the following features:
     """
-    def __init__(self, parent):
+    def __init__(self, parent, on_login_success_callback=None):
         self.parent = parent
+        self.on_login_success_callback = on_login_success_callback
         # Create the login frame
         self.login_frame = None
         self.user_frame = None
@@ -27,6 +26,10 @@ class LoginAndUserScreen():
         self.create_user_button = None
         self.forgot_password_button = None
         self.result_label = None
+        self.current_user = None
+
+        script_dir = os.path.dirname(__file__)
+        self.login_data = os.path.join(script_dir, "users.json")
 
     def create_login_screen(self):
         """
@@ -40,6 +43,7 @@ class LoginAndUserScreen():
         self.username_entry = ctk.CTkComboBox(self.login_frame, command=self.update_usernames_combobox) # pylint: disable=line-too-long
         self.username_entry.pack(pady=10)
         self.username_entry.set("")
+        self.update_usernames_combobox()
 
         self.password_label = ctk.CTkLabel(self.login_frame, text="Password")
         self.password_label.pack(pady=10)
@@ -65,18 +69,27 @@ class LoginAndUserScreen():
         """
         Function to read user data from file
         """
-        if os.path.exists(USER_DATA_FILE):
-            with open(USER_DATA_FILE, 'r', encoding='utf-8') as file:
+        if os.path.exists(self.login_data):
+            with open(self.login_data, 'r', encoding='utf-8') as file:
                 return json.load(file)
         return {}
 
     # Function to write user data to file
-    def write_user_data(self, data):
+    def write_user_data(self, username, password):
         """
         Function to write user data to file
         """
-        with open(USER_DATA_FILE, 'w', encoding='utf-8') as file:
-            json.dump(data, file)
+        # Assuming self.login_data is the path to the users.json file
+        if not os.path.exists(self.login_data):
+            data = {username: password}
+        else:
+            with open(self.login_data, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+                data[username] = password
+
+        with open(self.login_data, 'w', encoding='utf-8') as file:
+            json.dump(data, file, indent=4)
+
         self.update_usernames_combobox()
 
     # Function to update the ComboBox with usernames
@@ -86,7 +99,12 @@ class LoginAndUserScreen():
         """
         users = self.read_user_data()
         usernames = list(users.keys())
-        self.username_entry.configure(values=usernames)
+        print(users)
+        try:
+            self.username_entry.configure(values=usernames)
+        except AttributeError:
+            traceback.print_exc()
+        print(usernames)
 
     # Function to create a new user
     def create_user(self):
@@ -102,8 +120,7 @@ class LoginAndUserScreen():
                 self.result_label.configure(text="User already exists!", fg_color="red")
                 self.reset_label_text()
             else:
-                users[username] = password
-                self.write_user_data(users)
+                self.write_user_data(username, password)
                 self.result_label.configure(text="User created successfully!", fg_color="green")
                 self.reset_label_text()
         else:
@@ -121,16 +138,19 @@ class LoginAndUserScreen():
         if username and password:
             users = self.read_user_data()
             if username in users and users[username] == password:
+                self.current_user = username
                 self.result_label.configure(text="Login successful!", fg_color="green")
                 # Delay switching to the next screen by 2000 milliseconds (2 seconds)
-                self.result_label.after(1000, lambda: self.switch_to_user_screen(username))
+                self.result_label.after(1000, self.switch_to_user_screen())
             elif username not in users:
                 self.result_label.configure(text="User does not exist", fg_color="red")
+                self.reset_label_text()
             elif username in users and users[username] != password:
                 self.result_label.configure(text="Incorrect Password", fg_color="red")
+                self.reset_label_text()
             else:
                 self.result_label.configure(text="Invalid username or password", fg_color="red")
-            self.reset_label_text()
+                self.reset_label_text()
         else:
             self.result_label.configure(text="Please enter a username and password", fg_color="red")
             self.reset_label_text()
@@ -141,25 +161,25 @@ class LoginAndUserScreen():
         """
         self.result_label.after(3000, lambda: self.result_label.configure(text="", fg_color="black")) # pylint: disable=line-too-long
 
-    def enter_user(self, username):
+    def enter_user(self):
         """
         Function to enter a user
         """
-        self.switch_to_user_screen(username)
+        self.switch_to_user_screen()
 
     def forgot_password(self):
         """
         Function to reset the password of an existing user
         """
         username = self.username_entry.get()
+        password = self.password_entry.get()
         if username:
             users = self.read_user_data()
             if username in users:
                 dialog = ctk.CTkInputDialog(title="Reset Password", text="Enter your new password:") # pylint: disable=line-too-long
                 new_password = dialog.get_input()  # This waits for the user to input a new password # pylint: disable=line-too-long
-                if new_password and new_password != users[username]:  # Check if the user entered a new password # pylint: disable=line-too-long
-                    users[username] = new_password  # Update the password in the users dictionary
-                    self.write_user_data(users)  # Save the updated users dictionary back to the JSON file # pylint: disable=line-too-long
+                if new_password and new_password != password:  # Check if the user entered a new password # pylint: disable=line-too-long
+                    self.write_user_data(username, new_password)  # Save the updated users dictionary back to the JSON file # pylint: disable=line-too-long
                     self.result_label.configure(text="Password updated successfully!", fg_color="green") # pylint: disable=line-too-long
                     self.reset_label_text()
                 elif new_password == users[username]:  # Check if the new password is the same as the old password # pylint: disable=line-too-long
@@ -174,3 +194,17 @@ class LoginAndUserScreen():
         else:
             self.result_label.configure(text="Please enter a username", fg_color="red")
             self.reset_label_text()
+
+    def switch_to_user_screen(self):
+        """
+        Function to switch to the user screen
+        """
+        if self.on_login_success_callback:
+            self.on_login_success_callback()
+        self.login_frame.destroy()
+
+    def get_user_name(self):
+        """
+        Function to get the current user name
+        """
+        return self.current_user
--- main_snake_game.py
+++ main_snake_game.py
@@ -85,9 +85,6 @@ class SnakeGameApp:
         except FileNotFoundError as e:
             traceback.print_exc(e)
 
-        self.apply_theme()
-        self.update_contrast.apply_contrast(selected_value=None)
-
         # Write the changes to the config file
         try:
             with open(self.config_path, 'w', encoding='utf-8') as configfile:
@@ -225,7 +222,7 @@ class SnakeGameApp:
 
         # Initializing the button panel and label panel
 
-        self.login_screen = LoginAndUserScreen(self.root) # pylint: disable=line-too-long
+        self.login_screen = LoginAndUserScreen(self.root, on_login_success_callback=self.show_loading_screen_after_login) # pylint: disable=line-too-long
 
         self.create_button_panel = ClickButtonPanel(self.main_canvas, self.game_logger, self.functions) # pylint: disable=line-too-long
 
@@ -261,13 +258,21 @@ class SnakeGameApp:
         # Create the loading screen
         #self.create_loading_screen()
 
+        self.create_login_screen_main()
+
         self.root.protocol("WM_DELETE_WINDOW", self.confirm_quit)
 
-    def create_login_screen(self):
+    def create_login_screen_main(self):
         """
         Start the login screen.
         """
         self.login_screen.create_login_screen()
+
+    def show_loading_screen_after_login(self):
+        """
+        Show the loading screen after login.
+        """
+        self.create_loading_screen()
 
     def create_circle(self, canvas, x, y, r, **kwargs):
         """
@@ -286,12 +291,14 @@ class SnakeGameApp:
         """
         Create the loading screen.
         """
+        self.apply_theme()
+        self.update_contrast.apply_contrast(selected_value=None)
         self.original_main_canvas = self.main_canvas
         self.loading_canvas = ctk.CTkCanvas(self.root, bg='Grey20', highlightbackground='Black', highlightthickness=5) # pylint: disable=line-too-long
         self.loading_canvas.pack(expand=True, fill="both")
         self.loading_canvas.bind("<Configure>", self.update_loading_screen_position)
 
-        self.loading_canvas.after(1000, self.destroy_loading_screen)
+        self.loading_canvas.after(3000, self.destroy_loading_screen)
 
 
 
@@ -404,19 +411,19 @@ class SnakeGameApp:
         self.main_canvas.bind("<Configure>", self.update_text_position)
         # Create a CTkImage object
         self.create_and_place_image_label(self.main_canvas, 5, 635, self.config_path_icon)
-        # my_image = ctk.CTkImage(light_image=Image.open(self.config_path_icon),
-        #                         dark_image=Image.open(self.config_path_icon),
-        #                         size=(160, 160))
-
-        # # Create a CTkLabel to display the image
-        # image_label = ctk.CTkLabel(self.main_canvas, image=my_image, text="")
-
-        # Place the label at the bottom left of the window
-        # image_label.place(x=5, y=635)
         self.config.set('Settings', 'home_button_state', 'normal')
         self.config.set('Settings', 'classic_reset_high_score_button_state', 'normal')
         self.config.set('Settings', 'classic_reset_high_score_time_button_state', 'normal')
         self.config.set('Settings', 'classic_reset_high_score_snake_length_button_state', 'normal')
+
+        self.get_user_name = self.login_screen.get_user_name()
+        self.game_logger.log_game_event(f"User: {self.get_user_name}")
+
+        self.username_label = ctk.CTkLabel(self.main_canvas, text=f"User: {self.get_user_name}", font=("Helvetica", 20), fg_color="grey10", bg_color="Grey20") # pylint: disable=line-too-long
+        self.username_label.place(x=10, y=600)
+        #self.username_label.place(x=1100, y=10)
+        #self.username_label.place(x=1100, y=675)
+
         with open(self.config_path, 'w', encoding='utf-8') as configfile:
             self.config.write(configfile)
 
